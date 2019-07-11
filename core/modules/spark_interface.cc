c84db49e629ed79d6c66311e478fc1e1c5ecded6
Trevor Gale <trgale@sfu.ca>
2019-07-11 18:11:43 +0000

    WIP: Work on spark intergration

---
+++ core/modules/spark_interface.cc
@@ -0,0 +1,422 @@
+#include "spark_interface.h"
+
+static std::string HexDump(const void *buffer, size_t len);
+
+
+void parseMsgHdr(msgHdr_t* msgHdrData, uint8_t* ptr)
+{
+        msgHdrData->len_dat = *((bess::utils::be32_t*)(ptr + 0));
+        msgHdrData->msb_dat = *((bess::utils::be64_t*)(ptr + 4));
+        msgHdrData->lsb_dat = *((bess::utils::be64_t*)(ptr + 12));
+        msgHdrData->id_dat = *((bess::utils::be64_t*)(ptr + 20));
+        msgHdrData->type_dat = *((bess::utils::be32_t*)(ptr + 28));
+        msgHdrData->time_sec_dat = *((bess::utils::be64_t*)(ptr + 32));
+        msgHdrData->time_usec_dat = *((bess::utils::be64_t*)(ptr + 40));
+}
+
+void createBcdId(char *buf_p, const BcdID * bcd_id_p) {
+    BcdID *p = (BcdID *) buf_p;
+    p->app_id.msb = htobe64(bcd_id_p->app_id.msb);
+    p->app_id.lsb = htobe64(bcd_id_p->app_id.lsb);
+    p->data_id = htobe64(bcd_id_p->data_id);
+    return;
+}
+
+void createMsgHdr(char *buf_p, const BcdID *bcd_id_p, const msg_type_t type, const bytes_t len) {
+    MsgHdr *p = (MsgHdr *) buf_p;
+    createBcdId((char *) &(p->bcd_id), bcd_id_p);
+    p->type = htonl(type);
+    p->len = htonl(len);
+    gettimeofday(&(p->tv), NULL);
+    return;
+}
+
+bytes_t createMsg(char *buf_p, const BcdID *bcd_id_p, const msg_type_t type, const char *msg_payload, const bytes_t tcp_payload_len, const bytes_t padding_len) {
+    createMsgHdr(buf_p, bcd_id_p, type, tcp_payload_len + padding_len); /* assign the message header */
+    if (msg_payload) { /* copy the payload */
+        memcpy(buf_p + MSG_HDRLEN, msg_payload, tcp_payload_len - MSG_HDRLEN);
+    }
+    return tcp_payload_len;
+}
+
+static MsgToken *_createMsgToken(const bytes_t tcp_payload_len, uint8_t padding) {
+    MsgToken *mt_p = (MsgToken *) malloc(sizeof(MsgToken)); /* initiate the msg tuple */
+    mt_p->buf_p = (char *) malloc(tcp_payload_len); /* msg buffer for the entire message including the msg header and the payload */
+    mt_p->msg_len = tcp_payload_len;
+    mt_p->padding_len = padding ? (INTERFACE_MTU - IP4_HDRLEN - TCP_HDRLEN - mt_p->msg_len) : 0;
+    mt_p->byte_sent = 0;
+    gettimeofday(&(mt_p->tv), NULL);
+    return mt_p;
+}
+
+static MsgToken * createMsgToken(BcdID *bcd_id_p, msg_type_t type, char *msg_payload, bytes_t msg_payload_len, uint8_t padding) {
+    bytes_t tcp_payload_len = MSG_HDRLEN + msg_payload_len; // msgLen is the bytes in TCP payload
+	LOG(INFO) << "TCP Payload len: " << tcp_payload_len;
+    MsgToken * mt_p = _createMsgToken(tcp_payload_len, padding);
+    createMsg(mt_p->buf_p, bcd_id_p, type, msg_payload, tcp_payload_len, mt_p->padding_len);
+    return mt_p;
+}
+
+void deleteMsgToken(MsgToken *mt_p) {
+    if (!mt_p) {
+        // D("msgTuple is NULL");
+        return;
+    }
+    free(mt_p->buf_p);
+    free(mt_p);
+    return;
+}
+
+void BcdIdtoFilename(const BcdID *bcd_id_p, char *buf_p) {
+    sprintf(buf_p, "%sbroadcast-%08lx-%04lx-%04lx-%04lx-%012lx/broadcast_%ld", BCD_DIR_PREFIX, ((bcd_id_p->app_id.msb) & (0xffffffff00000000)) >> 32, ((bcd_id_p->app_id.msb) & (0x00000000ffff0000)) >> 16,
+        (bcd_id_p->app_id.msb) & (0x000000000000ffff), ((bcd_id_p->app_id.lsb) & (0xffff000000000000)) >> 48, (bcd_id_p->app_id.lsb) & (0x0000ffffffffffff), bcd_id_p->data_id);
+    return;
+}
+
+int SparkInterface::Resize(int slots) {
+  struct llring *old_queue = queue_;
+  struct llring *new_queue;
+
+  int bytes = llring_bytes_with_slots(slots);
+
+  new_queue =
+      reinterpret_cast<llring *>(aligned_alloc(alignof(llring), bytes));
+  if (!new_queue) {
+    return -ENOMEM;
+  }
+
+  int ret = llring_init(new_queue, slots, 0, 1);
+  if (ret) {
+    std::free(new_queue);
+    LOG(INFO) << "Init llring failed with ret: " << ret;
+    return -EINVAL;
+  }
+
+  /* migrate packets from the old queue */
+  if (old_queue) {
+    bess::Packet *pkt;
+
+    while (llring_sc_dequeue(old_queue, (void **)&pkt) == 0) {
+      ret = llring_sp_enqueue(new_queue, pkt);
+      if (ret == -LLRING_ERR_NOBUF) {
+        bess::Packet::Free(pkt);
+      }
+    }
+
+    std::free(old_queue);
+  }
+
+  queue_ = new_queue;
+  size_ = slots;
+
+  return 0;
+}
+
+
+
+CommandResponse SparkInterface::Init(const bess::pb::SparkInterfaceArg &arg){
+//    LOG(INFO) << "This is the Signal File Reader module INIT";
+//    LOG(INFO) << "Configured arg h_size: " << arg.h_size();
+//    h_size_ = arg.h_size();
+//
+    int ret = Resize(64);
+    if(ret){
+        return CommandFailure(-ret);
+    }
+//
+//
+//
+    if(RegisterTask(nullptr) == INVALID_TASK_ID){
+        return CommandFailure(ENOMEM, "Task creation failed");
+    }
+//
+    return CommandSuccess();
+}
+
+
+int SparkInterface::addMsgToQueue(BcdID *bcd_id_p, msg_type_t type, char *msg_payload, bytes_t msg_payload_len, uint8_t padding) {
+    MsgToken *mt_p = createMsgToken(bcd_id_p, type, msg_payload, msg_payload_len, padding);
+    return llring_enqueue(queue_, mt_p);
+}
+
+void SparkInterface::ProcessBatch(__attribute__((unused)) Context *ctx, bess::PacketBatch *batch){
+    //bess::PacketBatch *new_batch = ctx->task->AllocPacketBatch();
+    //new_batch->Copy(batch);
+    //batch->clear();
+    //RunNextModule(ctx, new_batch);
+
+    //Process the packet to open the file, place that onto the queue
+    //
+    //
+    int messagesSentCount = 0;
+
+    for(int i = 0; i < batch->cnt(); i++){
+        bess::Packet *pkt = batch->pkts()[i];
+        // char *ptr = pkt->buffer<char *>() + SNBUF_HEADROOM;
+        uint8_t *ptr = pkt->head_data<uint8_t*>(0);
+
+        int totalLength = pkt->total_len();
+
+        //First, let's read the standard parameter
+        //
+        //
+        //
+        msgHdr_t msgHdr_dat;
+        parseMsgHdr(&msgHdr_dat, ptr);
+
+
+        LOG(INFO) << "recvd len: " <<  msgHdr_dat.len_dat.value() << " total length: " << totalLength;
+        LOG(INFO) << "MSB data recv: " << std::hex <<  msgHdr_dat.msb_dat.value() << " LSB data recv: " << std::hex <<  msgHdr_dat.lsb_dat.value();
+        LOG(INFO) << "id data recv: " <<  msgHdr_dat.id_dat.value() << " type data recv: " <<  msgHdr_dat.type_dat.value();
+        LOG(INFO) << "time_sec_dat: " <<  msgHdr_dat.time_sec_dat.value() << " time_usec_dat: " <<  msgHdr_dat.time_usec_dat.value();
+
+        uint8_t buf[MAX_PKT_LEN - STD_HDR_LEN];
+        memcpy(buf, ptr+STD_HDR_LEN, msgHdr_dat.len_dat.value() - STD_HDR_LEN);
+
+        LOG(INFO) << "Memory sent: " << HexDump(buf, msgHdr_dat.len_dat.value() - STD_HDR_LEN);
+
+        //msgBytes* responseMsg = NULL;
+        MsgToken* responseMsg = NULL;
+        BcdID bcd_id_val;
+        bcd_id_val.app_id  = {msgHdr_dat.msb_dat.value(), msgHdr_dat.lsb_dat.value()};
+        bcd_id_val.data_id = msgHdr_dat.id_dat.value();
+
+        char msg[] = "TestMsg";
+		//bess::utils::be32_t responseCode;//(REPLY_SUCCESS);
+		int responseCode;
+        switch(msgHdr_dat.type_dat.value()){
+            case (MSG_INT_REQ):
+                LOG(INFO) << "";
+                //responseMsg =  createMsgToken(BcdID *bcd_id_p, msg_type_t type, char *msg_payload, bytes_t msg_payload_len, uint8_t padding) {
+				responseCode = htonl(REPLY_SUCCESS);
+                addMsgToQueue(&bcd_id_val, MSG_INT_RPL, (char*)&responseCode, sizeof(int), 0);
+                //responseMsg = createMsgToken(&bcd_id_val, MSG_INT_RPL, msg, strlen(msg), 0);
+                //llring_enqueue(queue_, responseMsg);
+
+                break;
+            case (MSG_WRT_REQ):
+                {
+                    LOG(INFO) << "Received MSG_WRT_REQ";
+                    char fullPath[FILENAME_LEN] = {0};
+                    BcdIdtoFilename(&bcd_id_val, fullPath);
+                    LOG(INFO) << "Going to use path of: " << fullPath;
+					LOG(INFO) << "Not sending reply";
+					responseCode = htonl(REPLY_SUCCESS);
+					addMsgToQueue(&bcd_id_val, MSG_WRT_RPL, (char*)&responseCode, sizeof(int), 0);
+                }
+                break;
+
+            default:
+                LOG(INFO) << "Received message of unknown type. ID: " << msgHdr_dat.type_dat.value();
+        }
+        //char pathBuf[PATH_MAX+1];
+
+        //if(totalLength > PATH_MAX){
+        //    LOG(INFO) << "Error, path too long. Length: " << totalLength;
+        //    // bess::Free(pkt);
+        //    continue;
+        //}
+        //else
+        //{
+        //    LOG(INFO) << "Total length valid: " << totalLength;
+        //}
+
+        //strncpy(pathBuf, ptr, totalLength);
+        //pathBuf[totalLength] = '\0';//Should be fixed -- no need to copy here.
+
+        // strcpy(sharedPath_, pathBuf);
+
+
+        //LOG(INFO) << "Packet contains: " << pathBuf;
+
+        //Now, read that file out
+        //if(strlen(pathBuf) == 0){
+        //    LOG(INFO) << "Invalid 0 length path.";
+        //    continue;
+        //}
+
+        //Dynamic array
+
+        //char *temporaryPathPtr = new char[totalLength+1];//Do this at initialization, don't dynamically allocate
+        //strcpy(temporaryPathPtr, pathBuf);
+        bess::Packet::Free(pkt);
+    }
+
+    //if(messagesSentCount == 0)
+    //{
+    //}
+}
+
+struct task_result SparkInterface::RunTask(
+  Context *ctx,
+  bess::PacketBatch *batch,
+  __attribute__((unused)) void *arg) {
+//
+//    static int lastOpenFd;
+//    static bool workingOnFd = false;
+//
+    struct task_result taskResultVal;
+    taskResultVal.packets = 0u;
+    taskResultVal.bits = 0;
+    taskResultVal.block = true;
+
+    llring_addr_t ringBufObj;
+    int pktSentCnt = 0;
+    if(llring_dequeue(queue_, &ringBufObj) == 0) {
+        LOG(INFO) << "Got item out of queue";
+        MsgToken* fromQueue = (MsgToken*)ringBufObj;
+        LOG(INFO) << "Got fromQueue: " << HexDump(fromQueue->buf_p, fromQueue->msg_len);
+        //bess::Packet *newPkt = current_worker.packet_pool()->Alloc();
+        int i = 0;
+        LOG(INFO) << "Initial count: " << batch->cnt();
+
+        char *newPtr = static_cast<char *>(batch->pkts()[i]->buffer()) + SNBUF_HEADROOM;
+
+        //Now we have to actually emit this data back to spark!
+        //memcpy(newPtr, memoryBuf, readSz);//Read directly into the packet -- don't copy
+        memcpy(newPtr, fromQueue->buf_p, fromQueue->msg_len);
+
+        batch->pkts()[i]->set_data_len(fromQueue->msg_len);
+        batch->pkts()[i]->set_total_len(fromQueue->msg_len);
+
+        //batch->add(newPkt);
+        pktSentCnt++;
+
+        RunNextModule(ctx, batch);
+
+        taskResultVal.packets = pktSentCnt;
+        taskResultVal.bits = 8*pktSentCnt;
+        taskResultVal.block = false;
+
+
+
+        
+        //LOG(INFO) << "Got: \"" << fromQueue->pkt << "\" len: " << fromQueue->len;
+    }
+
+//
+//
+//    llring_addr_t ringBufObj;
+//    bool workToDo = false;
+//
+//    static int totalSentPackets = 0;
+//
+//    if(workingOnFd){//In progress
+//        workToDo = true;
+//    }
+//    else if(llring_dequeue(queue_,  &ringBufObj) == 0){
+//        LOG(INFO) << "Got path from the queue!!!: " << (char*)ringBufObj;
+//
+//        if((lastOpenFd = open((char*)ringBufObj, O_RDONLY)) == -1){
+//            LOG(INFO) << "Failed to open file \"" << (char*)ringBufObj << "\"";
+//            workToDo = false;
+//        }
+//        else
+//        {
+//            workToDo = true;
+//        }
+//        LOG(INFO) << "Going to delete: \"" << (char*)ringBufObj << "\" " << (void*)ringBufObj;
+//        delete (char*)ringBufObj;
+//    }
+//
+//
+//    // llring_addr_t ringBufObj;
+//    // int dequeueRes = llring_dequeue(queue_,  ringBufObj);
+//    if(workToDo){
+//        // int lastOpenFd;
+//
+//        // if((lastOpenFd = open(local_working_path, O_RDONLY)) == -1){
+//        //     LOG(INFO) << "Failed to open file \"" << local_working_path << "\"";
+//        //     goto exitEmpty;
+//        // }
+//
+//        uint8_t memoryBuf[MAX_TOTAL_PACKET_SIZE];
+//        ssize_t readSz = -1; 
+//
+//        unsigned int pktSentCnt = 0u;
+//
+//        // lseek(lastOpenFd, last_path_offset, SEEK_SET);
+//        while(/*(readSz != 0) && */(! batch->full()) && (readSz = read(lastOpenFd, memoryBuf, MAX_TOTAL_PACKET_SIZE - h_size_)) > 0){
+//            //Allocate that packet and process it.
+//            // last_path_offset += readSz;
+//            bess::Packet *newPkt = current_worker.packet_pool()->Alloc();
+//
+//            // LOG(INFO) << "Allocated pkt: " << newPkt;
+//            // buf.add(pkt);
+//            char *newPtr = static_cast<char *>(newPkt->buffer()) + SNBUF_HEADROOM + h_size_;
+//
+//            memcpy(newPtr, memoryBuf, readSz);//Read directly into the packet -- don't copy
+//
+//
+//            newPkt->set_data_len(readSz);
+//            newPkt->set_total_len(readSz);
+//
+//            batch->add(newPkt);
+//            // RunNextModule(ctx, batch);
+//            pktSentCnt++;
+//        }
+//        totalSentPackets += pktSentCnt;
+//        if (readSz == 0) {
+//            // LOG(INFO) << "readSz == 0";
+//            // last_path_offset = 0;
+//            LOG(INFO) << "Total packets sent: " << totalSentPackets;
+//            totalSentPackets = 0;
+//            workingOnFd = false;
+//            if(close(lastOpenFd) != 0){
+//                LOG(INFO) << "Error could not close fd " << lastOpenFd;
+//            }
+//        }
+//        // LOG(INFO) << "Packet send cnt: " << pktSentCnt;
+//
+//        if(pktSentCnt > 0){
+//            RunNextModule(ctx, batch);
+//        }
+//
+//        taskResultVal.packets = pktSentCnt;
+//        taskResultVal.bits = 8*pktSentCnt;
+//        taskResultVal.block = false;
+//
+//        
+//        return taskResultVal;
+//    }
+//
+//
+//    // LOG(INFO) << "Task Result arg: " << arg << ctx << batch;
+    return taskResultVal;
+}
+
+
+
+
+
+// basically rte_hexdump() from eal_common_hexdump.c
+static std::string HexDump(const void *buffer, size_t len) {
+  std::ostringstream dump;
+  size_t i, ofs;
+  const char *data = reinterpret_cast<const char *>(buffer);
+
+  dump << "Dump data at [" << buffer << "], len=" << len << std::endl;
+  ofs = 0;
+  while (ofs < len) {
+    dump << std::setfill('0') << std::setw(8) << std::hex << ofs << ":";
+    for (i = 0; ((ofs + i) < len) && (i < 16); i++) {
+      dump << " " << std::setfill('0') << std::setw(2) << std::hex
+           << (data[ofs + i] & 0xFF);
+    }
+    for (; i <= 16; i++) {
+      dump << " | ";
+    }
+    for (i = 0; (ofs < len) && (i < 16); i++, ofs++) {
+      char c = data[ofs];
+      if ((c < ' ') || (c > '~')) {
+        c = '.';
+      }
+      dump << c;
+    }
+    dump << std::endl;
+  }
+  return dump.str();
+}
+
+
+ADD_MODULE(SparkInterface, "spark interface", "interface for commubication with spark")
